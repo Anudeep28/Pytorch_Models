9bf909f0fd4ad61d008d7e55a1bb0fd0b924bd26
Anudeep28 <anupatil28@gmail.com>
2024-07-25 10:07:30 +0530

    Model selection function and script

---
+++ going_modular/model_selection.py
@@ -0,0 +1,33 @@
+
+# Let us now functionize the model selection
+import torchvision
+from torch import nn
+
+def create_model(model_name: str,
+                 out_features: int=3):
+    assert model_name == "effnetb2" or model_name == "effnetv2_s", "Model name should be effnetb2 or effnetv2_s"
+    if model_name == "effnetb2":
+        weights = torchvision.models.EfficientNet_B2_Weights.DEFAULT
+        model = torchvision.models.efficientnet_b2(weights=weights)
+        dropout = 0.3
+        in_features = 1408
+    elif model_name == "effnetv2_s":
+        weights = torchvision.models.EfficientNet_V2_S_Weights.DEFAULT
+        model = torchvision.models.efficientnet_v2_s(weights=weights)
+        dropout = 0.2
+        in_features = 1280
+
+    # Freeze the base layer of the models
+    for param in model.parameters():
+        param.requires_grad = False
+
+    # Update the classifier head
+    model.classifier = nn.Sequential(
+        nn.Dropout(p=dropout, inplace=True),
+        nn.Linear(in_features=in_features,out_features=out_features)
+    )
+
+    # set the model name
+    model.name = model_name
+    print(f"[INFO] Creating {model_name} feature extractor model...")
+    return model
